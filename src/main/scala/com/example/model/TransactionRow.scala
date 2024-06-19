b918a2dbbf7e725f12a016e28a70b61b3dd0ed8d
twentyone37 <twentyone_37@proton.me>
2024-06-19 17:54:55 +0200

    T1: Resolve issue with TransactionRow and params for process

--- src/main/scala/com/example/model/TransactionRow.scala
+++ src/main/scala/com/example/model/TransactionRow.scala
@@ -13,10 +13,14 @@ case class TransactionRow(
 object TransactionRow {
 
   def apply(state: OrderRow, updated: OrderRow): TransactionRow = {
+    val amount =
+      if (state.filled == 0) updated.filled
+      else state.total - updated.filled
+
     TransactionRow(
       id = UUID.randomUUID(), // generate some id for our transaction
       orderId = state.orderId,
-      amount = state.filled,
+      amount = amount,
       createdAt = Instant.now()
     )
   }
